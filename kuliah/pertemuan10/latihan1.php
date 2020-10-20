ec6934853abb9f62d260a056b8cee15cd7a195d0
codeplacebo <codeplacebo@gmail.com>
2020-10-20 17:03:53 +0700

    membuat file latihan1.php

---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <!-- ------------------------------------------------------------------------
+    # bagian tabel
+    ------------------------------------------------------------------------- -->
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/sandhika.jpg" width="60" alt=""></td>
+      <td>170920021</td>
+      <td>Sadhika Galih</td>
+      <td>sadhikagalih@unpas.com</td>
+      <td>Teknik Informatika</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </table>
+
+</body>
+
+</html>
